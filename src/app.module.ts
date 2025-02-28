15fae79556666c586fe8940a1ac867d5f4b1b94c
nish1013 <nishand2d@gmail.com>
2025-02-28 06:55:30 +0000

    Refactor AppModule to integrate ConfigModule, MongooseModule, and ClaimsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ClaimsModule } from './claims/claims.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // Load environment variables
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
+    ClaimsModule,
+  ],
 })
 export class AppModule {}
