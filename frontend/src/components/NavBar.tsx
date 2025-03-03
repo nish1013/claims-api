07b2c3e0cd2e02676b578f551a439df49c12af26
nish1013 <nishand2d@gmail.com>
2025-03-03 08:06:30 +0000

    feat: Add API link to the navigation bar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -4,6 +4,13 @@ export function Navbar() {
       <a href="https://github.com/nish1013/claims-api" target="_blank" className="hover:underline">
         Code
       </a>
+      <a
+        href="https://claims-api-production.up.railway.app/api"
+        target="_blank"
+        className="hover:underline"
+      >
+        API
+      </a>
       <a href="https://satharasinghe.com" target="_blank" className="hover:underline">
         Contact
       </a>
