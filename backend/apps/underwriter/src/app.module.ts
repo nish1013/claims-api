7bcd8dc128ffb13a24705dc50f361c42baca9a63
nish1013 <nishand2d@gmail.com>
2025-03-06 08:42:53 +0000

    feat: Refactor underwriter application by removing AppController and AppService, and adding Claims module with controller and service

--- backend/apps/underwriter/src/app.module.ts
+++ backend/apps/underwriter/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ClaimsModule } from './claims/claims.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ClaimsModule],
 })
 export class AppModule {}
