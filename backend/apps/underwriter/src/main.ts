ac9204ad5cffd52203a0e53253d5597dbbd2c6e2
nish1013 <nishand2d@gmail.com>
2025-03-07 08:56:49 +0000

    feat: Add Swagger documentation for Underwriter API

--- backend/apps/underwriter/src/main.ts
+++ backend/apps/underwriter/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const config = new DocumentBuilder()
+    .setTitle('Underwriter API')
+    .setDescription('API for processing underwriting decisions')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
   const app = await NestFactory.create(AppModule);
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
