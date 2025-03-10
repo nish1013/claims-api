580d244e6c2b639b69f16f49a30bb064ab5732d9
nish1013 <nishand2d@gmail.com>
2025-03-10 08:36:48 +0000

    feat: Enable API versioning for Underwriter service

--- backend/apps/underwriter/src/main.ts
+++ backend/apps/underwriter/src/main.ts
@@ -1,16 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Underwriter API')
     .setDescription('API for processing underwriting decisions')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-
-  const app = await NestFactory.create(AppModule);
   
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document)
