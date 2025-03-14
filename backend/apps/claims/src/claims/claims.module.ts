7c955394b61ecde3cbc6fef5195c29989c547fb3
nish1013 <nishand2d@gmail.com>
2025-03-14 08:39:00 +0000

    feat: Add UnderwriterExternalService and ExternalAuthService to ClaimsModule providers

--- backend/apps/claims/src/claims/claims.module.ts
+++ backend/apps/claims/src/claims/claims.module.ts
@@ -3,13 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ClaimsController } from './claims.controller';
 import { ClaimsService } from './claims.service';
 import { Claim, ClaimSchema } from './schemas/claims.schema';
+import { UnderwriterExternalService } from 'src/underwriter/underwriter-external.service';
+import { ExternalAuthService } from 'src/security/external-auth.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Claim.name, schema: ClaimSchema }]),
   ],
   controllers: [ClaimsController],
-  providers: [ClaimsService],
+  providers: [ClaimsService, UnderwriterExternalService, ExternalAuthService],
   exports: [ClaimsService],
 })
 export class ClaimsModule {}
